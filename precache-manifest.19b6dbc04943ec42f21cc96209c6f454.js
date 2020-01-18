b1e1ffc3a4bfdb3bf293ce8f6034e6ea74048f9e
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2020-01-18 21:50:59 +0100

    Updates

---
+++ precache-manifest.19b6dbc04943ec42f21cc96209c6f454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7acc08ac903665c911b2f2b95612ee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56e4c33edc1f389e04e7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6496bf7c2d52f09a2f2",
+    "url": "/static/js/2.586e3ad1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.586e3ad1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56e4c33edc1f389e04e7",
+    "url": "/static/js/main.3aeea61a.chunk.js"
+  },
+  {
+    "revision": "6baac3510513a13616be",
+    "url": "/static/js/runtime-main.4846ab83.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
